6689f79f902a8998dd88be297e991088e512939c
lvivdev <kl@alushta.net>
2016-10-19 21:39:56 +0300

    Variable x in localstorage update

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 function something () 
 {
-	var x = window.localStorage.getItem('aaa');
+	var x = window.localStorage.getItem('bb');
+	x = x * 1 + 1;
 
-	window.localStorage.setItem('aaa', 555);
+	window.localStorage.setItem('bb', x);
 
 	alert(x);
 }
