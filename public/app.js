394c3a819da7aec06c3e1e154ba0ec36e2a77247
lvivdev <kl@alushta.net>
2016-10-22 13:36:24 +0300

    update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -10,5 +10,9 @@ function something ()
 
 function add_to_cart(id)
 {
-	alert('You added izza with id: ' + id);
+	var key = 'product_' + id;
+	
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
