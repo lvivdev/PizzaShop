09dcc6c0f01855fbe5093d7e648756bf97028d9f
lvivdev <kl@alushta.net>
2016-10-25 02:21:51 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -16,3 +16,20 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 }
+
+function cart_get_number_of_items ()
+{	
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); //getting key
+		var value = window.localStorage.getItem(key); // getting value
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+	return cnt;
+}
