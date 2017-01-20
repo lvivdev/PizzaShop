3a61f6b69002b4d70948383417917d7aff818fe6
lvivdev <kl@alushta.net>
2017-01-20 23:08:52 +0200

    Cart_get_orders function

--- public/app.js
+++ public/app.js
@@ -15,9 +15,17 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	update_orders_input();	
 }
 
-function cart_get_number_of_items ()
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+function cart_get_number_of_items()
 {	
 	var cnt = 0;
 
@@ -32,4 +40,22 @@ function cart_get_number_of_items ()
 		}
 	}
 	return cnt;
+}
+
+
+function cart_get_orders()
+{	
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); //getting key
+		var value = window.localStorage.getItem(key); // getting value
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+	return orders;
 }
