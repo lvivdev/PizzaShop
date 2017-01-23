62088a9dbf9675713a7786724e01e4038ca84de4
lvivdev <kl@alushta.net>
2017-01-23 23:04:31 +0200

    Add /cart view

--- app.rb
+++ app.rb
@@ -21,7 +21,8 @@ end
 post '/cart' do
 	orders_input = params[:orders]
 	@orders = parse_orders_input orders_input
-	erb "Hello #{@orders.inspect}"
+
+	erb :cart
 end
 
 def parse_orders_input orders_input
