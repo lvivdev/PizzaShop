8f34927cf78e70b7b159428c364033a40e696b29
lvivdev <kl@alushta.net>
2017-01-25 21:57:36 +0200

    Add Orders Model redone

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125174100) do
+ActiveRecord::Schema.define(version: 20170125195011) do
 
   create_table "orders", force: :cascade do |t|
+    t.text     "orders_input"
     t.text     "name"
     t.text     "phone"
     t.text     "address"
